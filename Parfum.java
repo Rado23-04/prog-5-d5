5bda1f596cb05ef02c3ea5f15076fcaabdeee7ee
Rado23-04 <hei.rado.2@gmail.com>
2025-05-28 09:26:19 +0300

    feat: add class Parfum coffee

---
+++ Parfum.java
@@ -0,0 +1,39 @@
+
+public class Parfum {
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Parfum(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    public boolean isAvailable(){
+        return quantity >0;
+    }
+}
